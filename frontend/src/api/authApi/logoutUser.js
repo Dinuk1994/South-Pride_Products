37a2a3370a814a75304afe9c96ae548a480cb40a
Dinuk1994 <ddt94119@gmail.com>
2024-09-30 03:35:19 +0530

    logout completed

---
+++ frontend/src/api/authApi/logoutUser.js
@@ -0,0 +1,26 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const logoutUser = createAsyncThunk(
+    'auth/logout' , async (thunkAPI) => {
+        try {
+            const res = await fetch("/api/auth/logout",{
+                method : "POST",
+                headers: {
+                    'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+                    'Expires': '0'
+                }
+            })
+
+            const data = await res.json();
+
+            if(!res.ok){
+                throw new Error(data.error)
+            }
+
+            return data;
+            
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message)
+        }
+    }
+)
--- frontend/src/components/admin/Header.jsx
+++ frontend/src/components/admin/Header.jsx
@@ -1,40 +1,76 @@
-
 import { IoMdLogOut } from "react-icons/io";
 import MobileDrawer from "./MobileDrawer";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import toast from "react-hot-toast";
+import { logoutUser } from "../../api/authApi/logoutUser";
 
 const Header = () => {
-  const userEmail = useSelector((state)=>state.auth.user.email)
+  const userEmail = useSelector((state) => state.auth.user.email);
+
+  const dispatch = useDispatch();
+
+  const handleLogOut = async () => {
+    try {
+      await dispatch(logoutUser()).unwrap();
+  
+      toast.success("Logout Successful");
+      window.location.href = "/auth/login"; 
+    } catch (error) {
+      toast.error("Logout Failed", error.message);
+      console.log(error);
+    }
+  };
+  
 
   return (
     <div>
-      <div className="navbar bg-AdminHeader ">
+      <div className="navbar bg-AdminHeader">
         <div className="flex-1">
-          <div className="desktop-or-laptop:hidden"><MobileDrawer /></div>
-          <a className="pl-3 text-xl mobile:ml-5 text-white mobile:text-sm font-bold">Administration - <span className="text-yellow-200 mobile:text-sm text-nowrap">South Pride Products</span></a>
+          <div className="desktop-or-laptop:hidden">
+            <MobileDrawer />
+          </div>
+          <a className="pl-3 text-xl mobile:ml-5 text-white mobile:text-sm font-bold">
+            Administration -{" "}
+            <span className="text-yellow-200 mobile:text-sm text-nowrap">
+              South Pride Products
+            </span>
+          </a>
         </div>
-        <div className="flex ">
+        <div className="flex">
           <div className="dropdown dropdown-end flex items-center">
-            <div tabIndex={0} role="button" className="btn btn-ghost  btn-circle avatar">
-              <div className="w-10 mobile:w-5  rounded-full">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 mobile:w-5 rounded-full">
                 <img
                   alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
               </div>
-            </div>       
+            </div>
           </div>
-          <label className="text-[15px] mobile:text-[10px] mobile:ml-0 ml-2 text-yellow-200" htmlFor="">{userEmail}</label>
+          <label
+            className="text-[15px] mobile:text-[10px] mobile:ml-0 ml-2 text-yellow-200"
+            htmlFor=""
+          >
+            {userEmail}
+          </label>
           <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn group btn-ghost btn-circle avatar hover:text-white hover:bg-red-500">
+            <div
+              tabIndex={0}
+              onClick={handleLogOut}
+              role="button"
+              className="btn group btn-ghost btn-circle avatar hover:text-white hover:bg-red-500"
+            >
               <IoMdLogOut className="text-2xl group" />
             </div>
           </div>
         </div>
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
